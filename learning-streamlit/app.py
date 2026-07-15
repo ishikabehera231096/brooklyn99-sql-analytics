73e6c4f9d748087ae6ba80219b90a6409bd918a8
Ishika Behera <ishikabehera23@gmail.com>
2026-07-15 14:26:49 +0530

    Created a dashboard with three tabs: rating trend, season comparison and cult favorite

--- learning-streamlit/app.py
+++ learning-streamlit/app.py
@@ -4,9 +4,17 @@ import plotly.express as px
 
 st.title("Brooklyn 99 Dashboard")
 
+#load all the datasets
 df = pd.read_csv("/Users/ishik/Desktop/brooklyn99-sql-analytics/exports/b99_episodes_cleaned.csv")
-st.write(f"Loaded {len(df)} episodes")
+running_avg_df = pd.read_csv("/Users/ishik/Desktop/brooklyn99-sql-analytics/exports/b99_running_avg_by_season.csv")
+improvement_df = pd.read_csv("/Users/ishik/Desktop/brooklyn99-sql-analytics/exports/b99_season_improvement.csv")
+df_cult_favorite = pd.read_csv("/Users/ishik/Desktop/brooklyn99-sql-analytics/exports/b99_cult_favorites.csv")
+
+
+
+st.write("Sample data")
 st.dataframe(df.head(10))
+st.write("Descriptive Statistics")
 st.write(df.describe())
 
 st.subheader("Rating Over Time")
@@ -14,60 +22,73 @@ st.subheader("Rating Over Time")
 #sorting the dataframe according to season and episode
 df_sorted = df.sort_values(["Season","Episode"])
 
-#ordering the episode_category column
-category_order = ["Weak","Average","Great","Masterpiece"]
-
 #creating a new column which shows episodes of each season as S<season>E<episode> format
 df_sorted["season_episode"] = "S" + df_sorted["Season"].astype(str) + "E" + df_sorted["Episode"].astype(str)
+
+#create a filter of all the charts in the dashboard
+st.sidebar.header("Filters")
+
+#create of list of seasons present in the show
 seasons = sorted(df_sorted["Season"].unique())
 
 #create a select box which shows all,1-8 seasons as selection value
-selected_season = st.selectbox("Select a Season",options=["All"]+list(seasons))
+selected_season = st.sidebar.selectbox("Select a Season",options=["All"]+list(seasons))
 
-#creating a logic for selection critrea
+#creating a logic for filter critrea
 if selected_season != "All":
     filtered_df = df_sorted[df_sorted["Season"] == selected_season]
 else:
-    filtered_df = df_sorted
+        filtered_df = df_sorted
+
+#create tabs for each chart
+tab1, tab2, tab3 = st.tabs(["📈 Rating Trend", "📊 Season Comparison", "💎 Cult Favorites"])
+
+with tab1:
+#ordering the episode_category column
+    category_order = ["Weak","Average","Great","Masterpiece"]
+
 
 #creating the line chart 
-fig = px.line(filtered_df,
-              x="season_episode",
-              y="Rating",
-              color="episode_category",
-              category_orders={"episode_category":category_order},
-              hover_data=["Title"],
-              title="Every Episode Rated",
-              markers=True)
-
+    color_map = {"Weak": "#08306b", "Average": "#4292c6", "Great": "#9ecae1", "Masterpiece": "#deebf7"}
+    fig = px.line(filtered_df,x="season_episode",y="Rating")
+    fig.update_traces(line_color = "gray")
+    fig.add_scatter(x=filtered_df["season_episode"],
+                    y=filtered_df["Rating"],
+                    mode="markers",
+                    marker=dict(color=filtered_df["episode_category"].map(color_map), size=10),
+                     text=filtered_df["Title"],
+                     hovertemplate="%{text}<br>Rating: %{y}<extra></extra>",
+                     showlegend=False
+                    )
+    
+    
 #fixing the x axis labels order 
-x_order = filtered_df["season_episode"].unique().tolist()
+    x_order = filtered_df["season_episode"].unique().tolist()
 
 #fix the tilt of the x axis labels 
-if selected_season == "All":
-    angle = 90 
-else:
-    angle = -45
+    if selected_season == "All":
+        angle = 90 
+    else:
+        angle = -45
 
-fig.update_xaxes(
-    categoryorder="array",
-    categoryarray=x_order,
-    tickangle = angle
-)
+    fig.update_xaxes(
+        categoryorder="array",
+        categoryarray=x_order,
+        tickangle = angle
+    )
 
 #fitting the chart to fit the container
-st.plotly_chart(fig,use_container_width=True)
+    st.plotly_chart(fig,use_container_width=True)
 
-
-# loading the season improvemement csv
-improvement_df = pd.read_csv("/Users/ishik/Desktop/brooklyn99-sql-analytics/exports/b99_season_improvement.csv")
+with tab2:
+    
 
 
 #putting chart in each column
-col1,col2 = st.columns(2)
+    col1,col2 = st.columns(2)
 
 #creating a bar and a histogram
-with col1:
+    with col1:
             st.subheader("Season Improvement")
             fig2 = px.bar(
                             improvement_df,
@@ -81,9 +102,9 @@ with col1:
             st.plotly_chart(fig2,use_container_width=True)
 
 
-with col2:
+    with col2:
             st.subheader("Rating Distribution")
-            fig3 = px.histogram(filtered_df,
+            fig3 = px.histogram(df,
                                 x="Rating",
                                 nbins=10,
                                 )
@@ -94,3 +115,18 @@ with col2:
 
 
 #load the cult favorite csv
+
+with tab3:
+    
+    st.subheader("Popularity vs. Appreciation")
+    fig4 = px.scatter(
+           df_cult_favorite,
+           x="Total Votes",
+           y="Rating",
+           color="reception_type",
+           size="Total Votes",
+           hover_data=["Title", "Season", "Episode"]
+       )
+    st.plotly_chart(fig4, use_container_width=True)
+    
+
