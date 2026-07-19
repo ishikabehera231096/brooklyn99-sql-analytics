a058588d41550ad8d15dc0905bced2ce1224ebac
ishikabehera231096 <55743361+ishikabehera231096@users.noreply.github.com>
2026-07-19 15:00:45 +0530

    Delete learning-api directory
    contained sensitive information

--- learning-api/json_to_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-with open("omdb_raw_results.json", "r") as f:
-    all_results = json.load(f)
-
-df_omdb = pd.DataFrame(all_results)
-df_omdb.to_csv("/Users/ishik/Desktop/brooklyn99-sql-analytics/data/omdb_raw.csv", index=False)
